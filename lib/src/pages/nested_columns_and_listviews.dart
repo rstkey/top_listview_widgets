050d7462dfd91a81cc5471f0b336b028f8c49efe
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 16:48:40 +0300

    Update nested_columns_and_listviews.dart

--- lib/src/pages/nested_columns_and_listviews.dart
+++ lib/src/pages/nested_columns_and_listviews.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
 class NestedColumnsAndListViewsPage extends StatelessWidget {
-  const NestedColumnsAndListViewsPage({Key? key, required this.title}) : super(key: key);
+  const NestedColumnsAndListViewsPage({Key? key, required this.title})
+      : super(key: key);
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text(title),
-
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 150,
+            color: Colors.red,
+            child: const Center(
+              child: Text('Some Widgets'),
+            ),
+          ),
+          Flexible(
+            child: ListView.builder(
+                itemCount: 30,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => ListTile(
+                      title: Text('Item ${index + 1}'),
+                    )),
+          )
+        ],
       ),
     );
   }
