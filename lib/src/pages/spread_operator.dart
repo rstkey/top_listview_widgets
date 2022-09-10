73d423ad55bb08a34ccce7fabb5e086c42c9b060
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 16:41:28 +0300

    Update spread_operator.dart

--- lib/src/pages/spread_operator.dart
+++ lib/src/pages/spread_operator.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
-class SpreadOperatorPage extends StatelessWidget {
+class SpreadOperatorPage extends StatefulWidget {
   const SpreadOperatorPage({Key? key, required this.title}) : super(key: key);
   final String title;
+
+  @override
+  State<SpreadOperatorPage> createState() => _SpreadOperatorPageState();
+}
+
+class _SpreadOperatorPageState extends State<SpreadOperatorPage> {
+  final items=const[Text('Item 2'),Text('Item3')];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(24),
+          children: [
+            const Text('Item 1'),
+            Column(children: items,),
+            const Text('Item 4'),
+          ],
+        ),
       ),
     );
   }
