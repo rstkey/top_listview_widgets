5ec9741e8839c14f4d746981f34943f0b0dc0479
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 17:17:53 +0300

    Update single_child_scroll_view.dart

--- lib/src/pages/single_child_scroll_view.dart
+++ lib/src/pages/single_child_scroll_view.dart
@@ -7,28 +7,65 @@ class SingleChildScrollViewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.arrow_forward)),
+              Tab(icon: Icon(Icons.arrow_downward_sharp)),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            Container(
-              alignment: Alignment.center,
-              color: Colors.red[100],
-              height: 200,
-              child: const Text('Item 1'),
-            ),
-            Container(
-              alignment: Alignment.center,
-              color: Colors.blue[100],
-              height: 500,
-              child: const Text('Item 2'),
-            )
+            horizontal(context),
+            vertical(context),
           ],
         ),
       ),
     );
   }
+}
+
+horizontal(context) {
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+      children: [
+        customContainer('Item 1', Colors.red[100], context),
+        customContainer('Item 2', Colors.blue[100], context),
+        customContainer('Item 3', Colors.green[100], context),
+      ],
+    ),
+  );
+}
+
+Container customContainer(text, color, context,
+    {double? height, bool? isVertical}) {
+  return Container(
+    alignment: Alignment.center,
+    color: color,
+    height: !(isVertical ?? false)
+        ? MediaQuery.of(context).size.height
+        : height ?? 200,
+    width: isVertical ?? false ? MediaQuery.of(context).size.width : 200,
+    child: Text(text),
+  );
+}
+
+vertical(context) {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        customContainer('Item 1', Colors.red[100], context, isVertical: true),
+        customContainer('Item 2', Colors.blue[100], context,
+            height: 500, isVertical: true),
+        customContainer('Item 3', Colors.purple[100], context,
+            height: 150, isVertical: true),
+      ],
+    ),
+  );
 }
