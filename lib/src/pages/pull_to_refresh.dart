d94cb6a6933f34083e92fead45614545bcd59c04
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 14:44:41 +0300

    Update pull_to_refresh.dart

--- lib/src/pages/pull_to_refresh.dart
+++ lib/src/pages/pull_to_refresh.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
-class PullToRefreshPage extends StatelessWidget {
+class PullToRefreshPage extends StatefulWidget {
   const PullToRefreshPage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
+  State<PullToRefreshPage> createState() => _PullToRefreshPageState();
+}
+
+class _PullToRefreshPageState extends State<PullToRefreshPage> {
+  List<String> items = ["dlfksjf", "lkodsjıf", "kfjkd", "ksdjh"];
+
+  Future refresh() async {
+    setState(() {
+      items = ["1", "2", "3", "4"];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text(title),
-      
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        onRefresh: refresh,
+        child: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+              return ListTile(
+                title: Text(item),
+              );
+            }),
       ),
     );
   }
