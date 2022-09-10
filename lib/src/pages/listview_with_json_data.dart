34bf753ee43e14ce9a98a5c21c98a374759da98b
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 19:39:00 +0300

    DefaultAssetBundle

--- lib/src/pages/listview_with_json_data.dart
+++ lib/src/pages/listview_with_json_data.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:top_listview_widgets/src/models/Book.dart';
 
@@ -12,38 +14,19 @@ class ListViewWithJsonDataPage extends StatefulWidget {
 }
 
 class _ListViewWithJsonDataPageState extends State<ListViewWithJsonDataPage> {
-  Future<List<Book>> books = getBooks();
-  static Future<List<Book>> getBooks() async {
-    const data = [
-      {
-        "title": "Fyunli (A Garhwali Haiku)",
-        "name": "Sunil Uniyal",
-        "urlAvatar": "https://www.poemist.com/images/poet-pen.png"
-      },
-      {
-        "title": "Lament XVII",
-        "name": "Jan Kochanowski",
-        "urlAvatar": "https://www.poemist.com/images/poet-pen.png"
-      },
-      {
-        "title": "In The Valley Of The Waters",
-        "name": "George Gordon Byron",
-        "urlAvatar":
-            "https://www.poemist.com/storage/poets-avatars/202206/george-gordon-byron.jpg"
-      },
-      {
-        "title": "Bekjendelser",
-        "name": "Bernhard Severi Ingemann",
-        "urlAvatar": "https://www.poemist.com/images/poet-pen.png"
-      },
-      {
-        "title": "The Boy’s Appeal -",
-        "name": "Rosanna Eleanor Leprohon",
-        "urlAvatar": "https://www.poemist.com/images/poet-pen.png"
-      }
-    ];
+  late Future<List<Book>> books;
 
-    return data.map<Book>(Book.fromJson).toList();
+  @override
+  void initState() {
+    super.initState();
+    books = getBooks(context);
+  }
+
+  static Future<List<Book>> getBooks(BuildContext context) async {
+    final assetBundle = DefaultAssetBundle.of(context);
+    final data = await assetBundle.loadString('assets/books.json');
+    final body = json.decode(data);
+    return body.map<Book>(Book.fromJson).toList();
   }
 
   @override
