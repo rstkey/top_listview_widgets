3d642cdf6eb1ecd84b491f7cf23ea09d4d3bdf5f
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 23:38:53 +0300

    Update reorderable_listview.dart

--- lib/src/pages/reorderable_listview.dart
+++ lib/src/pages/reorderable_listview.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
-class ReorderableListViewPage extends StatelessWidget {
-  const ReorderableListViewPage({Key? key, required this.title}) : super(key: key);
+class ReorderableListViewPage extends StatefulWidget {
+  const ReorderableListViewPage({Key? key, required this.title})
+      : super(key: key);
   final String title;
+
+  @override
+  State<ReorderableListViewPage> createState() =>
+      _ReorderableListViewPageState();
+}
+
+class _ReorderableListViewPageState extends State<ReorderableListViewPage> {
+  final items = ["Item 1", "Item 2", "Item 3", "Item 4"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text(title),
-
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ReorderableListView(
+        onReorder: (oldIndex, newIndex) {
+          setState(() {
+            if (newIndex > oldIndex) newIndex--;
+            final item = items.removeAt(oldIndex);
+            items.insert(newIndex, item);
+          });
+        },
+        children: [
+          for (final item in items)
+            ListTile(
+              key: ValueKey(item),
+              title: Text(item),
+            ),
+        ],
       ),
     );
   }
