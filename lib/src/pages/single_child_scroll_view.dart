3d21983d42bbfe2c14eddff7b4135a7a93895e95
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 16:54:50 +0300

    Update single_child_scroll_view.dart

--- lib/src/pages/single_child_scroll_view.dart
+++ lib/src/pages/single_child_scroll_view.dart
@@ -11,22 +11,23 @@ class SingleChildScrollViewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            color: Colors.red[100],
-            height: 200,
-            child: const Text('Item 1'),
-          ),
-          Expanded(
-              child: Container(
-            alignment: Alignment.center,
-            color: Colors.blue[100],
-            height: 500,
-            child: const Text('Item 2'),
-          ))
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              color: Colors.red[100],
+              height: 200,
+              child: const Text('Item 1'),
+            ),
+            Container(
+              alignment: Alignment.center,
+              color: Colors.blue[100],
+              height: 500,
+              child: const Text('Item 2'),
+            )
+          ],
+        ),
       ),
     );
   }
