3384281a386f21c73ca0eee22cddcb92657fd7c4
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 15:06:41 +0300

    Update infinite_scrolling_listview.dart
    kaydırma çubuğuna circular progres indicator eklendi

--- lib/src/pages/infinite_scrolling_listview.dart
+++ lib/src/pages/infinite_scrolling_listview.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class InfiniteScrollingListViewPage extends StatelessWidget {
-  const InfiniteScrollingListViewPage({Key? key, required this.title}) : super(key: key);
+class InfiniteScrollingListViewPage extends StatefulWidget {
+  const InfiniteScrollingListViewPage({Key? key, required this.title})
+      : super(key: key);
   final String title;
+
+  @override
+  State<InfiniteScrollingListViewPage> createState() =>
+      _InfiniteScrollingListViewPageState();
+}
+
+class _InfiniteScrollingListViewPageState
+    extends State<InfiniteScrollingListViewPage> {
+  List<String> items = List.generate(15, (index) => 'Item ${index + 1}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text(title),
-
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
+      body: ListView.builder(
+          padding:const EdgeInsets.all(8),
+          itemCount: items.length + 1,
+          itemBuilder: (context, index) {
+            if (index < items.length) {
+              final item = items[index];
+              return ListTile(
+                title: Text(item),
+              );
+            }else{
+              return const Padding(padding: EdgeInsets.symmetric(vertical: 32),child: Center(child: CircularProgressIndicator(),),);
+            }
+          }),
     );
   }
 }
