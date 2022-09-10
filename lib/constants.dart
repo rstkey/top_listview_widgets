365e6c437b3d8cb2f056731a0f4730496183ec02
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 13:57:08 +0300

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+const List<String> items = [
+  "Pull To Refresh",
+  "Infinite Scrolling ListView",
+  "Spread Operator",
+  "Nested Columns and ListViews",
+  "SingleChildScrollView",
+  "ListView With JSON Data",
+  "Sticky Header",
+  "Search & Filter ListView",
+  "Reorderable ListView",
+  "ListView With Navigator",
+  "Horizontal ListView",
+  "Vertical ListView",
+  "Grouped ListView"
+];
