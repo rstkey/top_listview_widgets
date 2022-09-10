9969e8cd00a8692faea17cabc9b555e32156628a
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 13:57:15 +0300

    Update home.dart

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:top_listview_widgets/constants.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TOP 13 LISTVIEW WIDGETS"),
+      ),
+      body: ListView.builder(
+          itemCount: 13,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return ListTile(
+              title: customCard(index, item),
+            );
+          }),
+    );
+  }
+
+  Card customCard(int index, String item) {
+    return Card(
+      color: (index % 2 == 0) ? Colors.green[100] : Colors.purple[100],
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          item,
+          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+        ),
+      ),
     );
   }
 }
