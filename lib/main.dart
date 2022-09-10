5d131b6f67500d4103a14a012bfa15b29f8b3376
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 13:57:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
