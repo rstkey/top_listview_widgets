c47d3996f6d12f6dd31061d4a95b5393238d101d
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-10 16:53:47 +0300

    Update single_child_scroll_view.dart

--- lib/src/pages/single_child_scroll_view.dart
+++ lib/src/pages/single_child_scroll_view.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
 class SingleChildScrollViewPage extends StatelessWidget {
-  const SingleChildScrollViewPage({Key? key, required this.title}) : super(key: key);
+  const SingleChildScrollViewPage({Key? key, required this.title})
+      : super(key: key);
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text(title),
-
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            color: Colors.red[100],
+            height: 200,
+            child: const Text('Item 1'),
+          ),
+          Expanded(
+              child: Container(
+            alignment: Alignment.center,
+            color: Colors.blue[100],
+            height: 500,
+            child: const Text('Item 2'),
+          ))
+        ],
       ),
     );
   }
