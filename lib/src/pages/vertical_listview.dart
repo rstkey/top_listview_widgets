8daab3ee69f8901a4131aa766addb0fc12708151
Nikola318 <nikola38.jovanovic@gmail.com>
2022-09-11 19:03:23 +0300

    Update vertical_listview.dart

--- lib/src/pages/vertical_listview.dart
+++ lib/src/pages/vertical_listview.dart
@@ -1,14 +1,68 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class VerticalListViewPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:top_listview_widgets/src/models/image.dart' as model;
+import 'package:http/http.dart' as http;
+import 'package:top_listview_widgets/urls.dart';
+
+class VerticalListViewPage extends StatefulWidget {
   const VerticalListViewPage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
+  State<VerticalListViewPage> createState() => _VerticalListViewPageState();
+}
+
+class _VerticalListViewPageState extends State<VerticalListViewPage> {
+  late Future<List<model.Image>> images;
+  final String defaultImageUrl =
+      'https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80';
+
+  @override
+  void initState() {
+    super.initState();
+    images = getImages();
+  }
+
+  static Future<List<model.Image>> getImages() async {
+    const url =
+        "https://api.unsplash.com/search/photos?query=london&client_id=$clientId";
+    final response = await http.get(Uri.parse(url));
+    final body = jsonDecode(response.body);
+    return body['results'].map<model.Image>(model.Image.fromJson).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text(title),
-
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder<List<model.Image>>(
+        future: getImages(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final images = snapshot.data!;
+            return ListView.builder(
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  final image = images[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          image.raw ?? defaultImageUrl,
+                          fit: BoxFit.cover,
+                          height: 200,
+                        )),
+                  );
+                });
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
